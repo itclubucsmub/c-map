43bc365a869eaf2701230aec8e9cd8b02fe78570
wailinnkyaw <wailinkyaw0011@gmail.com>
2019-02-21 15:59:19 +0630

    21-2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
-  <div>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title class="headline">
-        <span>C-map  </span>
-        <span class="font-weight-light">your pocket guide</span>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>C-map</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <span>
+        <v-btn router-link to="/" flat>Home</v-btn>
+        <v-btn router-link to="/sugtest" flat>Magic Bar</v-btn>
+
+        <v-menu bottom origin="center center" transition="scale-transition">
+          <v-btn slot="activator" color="primary" dark>Contributor</v-btn>
+
+          <v-list>
+            <v-list-tile v-for="(item, i) in items" :key="i" @click ="items">
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+
+      </span>
     </v-toolbar>
+
     <v-content>
-      <router-view></router-view>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-content>
   </v-app>
-  </div>
 </template>
 
 <script>
@@ -20,7 +35,10 @@ export default {
   name: "App",
   data() {
     return {
-
+      items: [
+        { title: "Add LaptopInfo"},
+        { title: "Add App" }
+      ]
     };
   }
 };
